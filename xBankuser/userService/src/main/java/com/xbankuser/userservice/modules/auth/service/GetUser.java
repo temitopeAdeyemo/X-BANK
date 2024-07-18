f82f2611806a9d21a8c2175362c17608b3f5a592
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-07-18 12:31:10 +0100

    tested and clean up code

--- xBankuser/userService/src/main/java/com/xbankuser/userservice/modules/auth/service/GetUser.java
+++ xBankuser/userService/src/main/java/com/xbankuser/userservice/modules/auth/service/GetUser.java
@@ -5,28 +5,16 @@ import com.xbankuser.userservice.modules.auth.repository.UserRepository;
 import com.xbankuser.userservice.shared.exception.UserNotFoundException;
 import com.xbankuser.userservice.shared.mapper.UserDataMapper;
 import io.grpc.stub.StreamObserver;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.*;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
 import proto.getUser.proto.GetAllUsersRequest;
 import proto.getUser.proto.GetAllUsersResponse;
 import proto.getUser.proto.GetUserByUniqueFieldRequest;
 import proto.getUser.proto.User;
 import proto.service.proto.GetUserServiceGrpc;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @GrpcService
@@ -34,7 +22,6 @@ public class GetUser extends GetUserServiceGrpc.GetUserServiceImplBase {
     private final UserRepository userRepository;
     @Override
     public void getUserByUniqueField(GetUserByUniqueFieldRequest request, StreamObserver<User> responseObserver) {
-        System.out.println("......................"+ request.getValue());
         var user = this.userRepository.findByUniqueData(request.getValue()).orElseThrow(()->new UserNotFoundException("User Not Found"));
 
         User userBuild = UserDataMapper.mapUserToProtobuf(user);
@@ -51,7 +38,6 @@ public class GetUser extends GetUserServiceGrpc.GetUserServiceImplBase {
         PageRequest pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
         var probe = new com.xbankuser.userservice.modules.auth.entiy.User();
         User filter = request.getFilter();
-//        System.out.println(Role.valueOf(filter.getRole())+".................."+probe.getRole());
 
         probe.setLastName(filter.getLastName());
         probe.setFirstName(filter.getFirstName());
@@ -59,35 +45,14 @@ public class GetUser extends GetUserServiceGrpc.GetUserServiceImplBase {
         probe.setPhoneNumber(filter.getPhoneNumber());
         if(!filter.getRole().isEmpty()) probe.setRole(Role.valueOf(filter.getRole()));
 
-//        if(!filter.getLastName().isEmpty()) probe.setLastName(filter.getLastName());
-//        if(!filter.getFirstName().isEmpty()) probe.setFirstName(filter.getFirstName());
-////        if(!filter.getId().isEmpty()) probe.setId(UUID.fromString(filter.getId()));
-//        if(!filter.getEmail().isEmpty()) probe.setEmail(filter.getEmail());
-//        if(!filter.getPhoneNumber().isEmpty()) probe.setPhoneNumber(filter.getPhoneNumber());
-//        if(!filter.getRole().isEmpty()) probe.setRole(Role.valueOf(filter.getRole()));
-
         ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("id", "emailVerified")
                 .withIgnoreCase()
                 .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING)
                 .withIgnoreNullValues();
 //                .withIncludeNullValues();
 
-
         Example<com.xbankuser.userservice.modules.auth.entiy.User> ex = Example.of(probe, matcher);
 
-//        System.out.println("..."+ex.getProbe());
-
-//        Page<com.xbankuser.userservice.modules.auth.entiy.User> users = this.userRepository.findAllByEmailOrLastNameOrPhoneNumberOrRoleOrEmailVerified(
-//                filter.getEmail(),
-//                filter.getLastName(),
-//                filter.getPhoneNumber(),
-////                UUID.fromString(filter.getId()),
-//                Role.valueOf(filter.getRole()),
-//                filter.getEmailVerified(),
-//                pageable
-//                );
-
-        System.out.println("::::::::::::::::::::::::");
         Page<com.xbankuser.userservice.modules.auth.entiy.User> users = this.userRepository.findAll(ex, pageable);
 
         List<User> userList = new ArrayList<>();
@@ -96,7 +61,7 @@ public class GetUser extends GetUserServiceGrpc.GetUserServiceImplBase {
             User userBuild = UserDataMapper.mapUserToProtobuf(user);
             userList.add(userBuild);
         }
-        System.out.println(Arrays.toString(userList.toArray()));
+
         responseObserver.onNext(GetAllUsersResponse.newBuilder().addAllUsers(userList).build());
         responseObserver.onCompleted();
     }
