52ea277916d630f227292ef1a2bf378317806902
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-07-19 06:46:12 +0100

    created test service to test server

--- wallet/walletService/src/main/java/com/xbank/walletservice/modules/walletManagement/service/SayHello.java
+++ wallet/walletService/src/main/java/com/xbank/walletservice/modules/walletManagement/service/SayHello.java
@@ -1,12 +1,15 @@
 package com.xbank.walletservice.modules.walletManagement.service;
 
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import proto.service.proto.WalletServiceGrpc;
 import proto.wallet.proto.SayHelloResponse;
 
+@GrpcService
 public class SayHello extends WalletServiceGrpc.WalletServiceImplBase {
     @Override
     public void sayHelloService(proto.wallet.proto.SayHello request, StreamObserver<SayHelloResponse> responseObserver) {
-        
+        responseObserver.onNext(SayHelloResponse.newBuilder().setMessage("Hello World").build());
+        responseObserver.onCompleted();
     }
 }
