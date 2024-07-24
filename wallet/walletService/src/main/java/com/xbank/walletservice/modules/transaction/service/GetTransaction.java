48b04efe52e6bc24ee2f2f433b70a94859fcbb9a
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-07-24 16:13:20 +0100

    refactored code

--- wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/GetTransaction.java
+++ wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/GetTransaction.java
@@ -1,9 +1,13 @@
 package com.xbank.walletservice.modules.transaction.service;
 
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
 import proto.service.proto.GetTransactionServiceGrpc;
 import proto.transaction.proto.*;
 
+@RequiredArgsConstructor
+@GrpcService
 public class GetTransaction extends GetTransactionServiceGrpc.GetTransactionServiceImplBase {
     @Override
     public void getSingleTransaction(GetSingleTransactionRequest request, StreamObserver<Transaction> responseObserver) {
--- wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/TransactionInit.java
+++ wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/TransactionInit.java
@@ -1,13 +1,19 @@
 package com.xbank.walletservice.modules.transaction.service;
 
+import com.xbank.walletservice.modules.transaction.repository.TransactionRepository;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
 import proto.service.proto.TransactionInitServiceGrpc;
 import proto.transaction.proto.FundTransferRequest;
 import proto.transaction.proto.FundTransferResponse;
 import proto.transaction.proto.FundWalletRequest;
 import proto.transaction.proto.StatusResponse;
 
+@RequiredArgsConstructor
+@GrpcService
 public class TransactionInit extends TransactionInitServiceGrpc.TransactionInitServiceImplBase {
+    private final TransactionRepository transactionRepository;
     @Override
     public void fundTransfer(FundTransferRequest request, StreamObserver<FundTransferResponse> responseObserver) {
 
