5888ff8b16abe13f2b0e00b91100ba84b9866b85
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-07-24 16:10:24 +0100

    set up services to fund wallet, transfer and manage transactions

--- wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/GetTransaction.java
+++ wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/GetTransaction.java
@@ -1,4 +1,22 @@
 package com.xbank.walletservice.modules.transaction.service;
 
-public class GetTransaction {
+import io.grpc.stub.StreamObserver;
+import proto.service.proto.GetTransactionServiceGrpc;
+import proto.transaction.proto.*;
+
+public class GetTransaction extends GetTransactionServiceGrpc.GetTransactionServiceImplBase {
+    @Override
+    public void getSingleTransaction(GetSingleTransactionRequest request, StreamObserver<Transaction> responseObserver) {
+
+    }
+
+    @Override
+    public void getBalance(GetBalanceRequest request, StreamObserver<GetBalanceResponse> responseObserver) {
+
+    }
+
+    @Override
+    public void getAllTransactions(GetAllTransactionsRequest request, StreamObserver<GetAllTransactionsResponse> responseObserver) {
+
+    }
 }
--- wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/TransactionInit.java
+++ wallet/walletService/src/main/java/com/xbank/walletservice/modules/transaction/service/TransactionInit.java
@@ -1,4 +1,20 @@
 package com.xbank.walletservice.modules.transaction.service;
 
-public class TransactionInit {
+import io.grpc.stub.StreamObserver;
+import proto.service.proto.TransactionInitServiceGrpc;
+import proto.transaction.proto.FundTransferRequest;
+import proto.transaction.proto.FundTransferResponse;
+import proto.transaction.proto.FundWalletRequest;
+import proto.transaction.proto.StatusResponse;
+
+public class TransactionInit extends TransactionInitServiceGrpc.TransactionInitServiceImplBase {
+    @Override
+    public void fundTransfer(FundTransferRequest request, StreamObserver<FundTransferResponse> responseObserver) {
+
+    }
+
+    @Override
+    public void fundWallet(FundWalletRequest request, StreamObserver<StatusResponse> responseObserver) {
+
+    }
 }
