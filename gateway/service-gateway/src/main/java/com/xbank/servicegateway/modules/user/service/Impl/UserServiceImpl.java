fbb9e9ac1ca2aeac85a099b5a07feb2511aef5b5
temitopeAdeyemo <temitopejulius9@gmail.com>
2024-08-06 14:13:27 +0100

    Refactored grpc authentication

--- gateway/service-gateway/src/main/java/com/xbank/servicegateway/modules/user/service/Impl/UserServiceImpl.java
+++ gateway/service-gateway/src/main/java/com/xbank/servicegateway/modules/user/service/Impl/UserServiceImpl.java
@@ -2,9 +2,10 @@ package com.xbank.servicegateway.modules.user.service.Impl;
 
 import com.xbank.servicegateway.modules.user.service.UserService;
 import com.xbank.servicegateway.shared.service.UserClient;
-import lombok.AllArgsConstructor;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import proto.user.proto.SignupRequest;
 import proto.user.proto.SignupResponse;
 
@@ -16,7 +17,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public SignupResponse registerUser() {
-        
+
         return this.userClient.registerUser(SignupRequest.newBuilder().build());
     }
 }
